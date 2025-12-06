1e952db12c683f078776e6ff96bac6740082408f
mylenemelo <mhyemelo@yahoo.com>
2025-12-05 23:11:31 -0500

    Add files via upload

---
+++ JavaScript Repository/Basic JavaScript Project/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+// Basic JavaScript Project: Project1_expressions_alert
+
+// Display an alert message to the user
+window.alert("Welcome to my JavaScript project!");
+
+// Declare two string variables
+var Sent1 = "This is the beginning of the string"; // First part of the sentence
+var Sent2 = " and this is the end of the string";  // Second part of the sentence
+
+// Concatenate the two variables
+var fullSentence = Sent1 + Sent2;
+
+// Write the concatenated string to the browser
+document.write(fullSentence);
+
+// Example of an expression
+var num1 = 10;
+var num2 = 5;
+var sum = num1 + num2; // Expression: adding two numbers
+document.write("<br>The sum of " + num1 + " + " + num2 + " is " + sum + ".");
