e54fd39c776d6bbe5039a413304ac1050960e979
mylenemelo <mhyemelo@yahoo.com>
2025-12-07 20:31:46 -0500

    Delete Project2_functions directory

--- Project2_functions/JS/main.js
+++
@@ -1,8 +0,0 @@
-function addText() {
-
-    let message = "This project demonstrates ";
-
-    message += "how the += operator works in JavaScript.";
-
-    document.getElementById("outputText").innerHTML = message;
-}
